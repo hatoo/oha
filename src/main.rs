ea39b5ffd813cf09797363066a2efac28162b2b8
hatoo <hato2000@gmail.com>
2020-03-03 16:51:38 +0900

    clap

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,12 @@ use url::Url;
 #[derive(Clap)]
 #[clap(version = "0.0.0", author = "hatoo")]
 struct Opts {
+    #[clap(help = "URL to request")]
     url: String,
+    #[clap(help = "Number of requests", short = "n", default_value = "200")]
+    n_requests: usize,
+    #[clap(help = "Number of workers", short = "c", default_value = "50")]
+    n_workers: usize,
 }
 
 #[tokio::main]
@@ -13,12 +18,9 @@ async fn main() -> anyhow::Result<()> {
     let url = Url::parse(opts.url.as_str())?;
     let client = reqwest::Client::new();
 
-    let c: usize = 50;
-    let n: usize = 200;
-
     let mut tasks = Vec::new();
 
-    for _ in 0..n {
+    for _ in 0..opts.n_requests {
         tasks.push(async {
             let resp = client.get(url.clone()).send().await?;
             let status = resp.status();
@@ -29,17 +31,17 @@ async fn main() -> anyhow::Result<()> {
 
     let start = std::time::Instant::now();
 
-    let res = work_tasks(tasks, c).await;
+    let res = work(tasks, opts.n_workers).await;
     let duration = std::time::Instant::now() - start;
     dbg!(res.into_iter().map(|v| v.len()).collect::<Vec<_>>());
 
     dbg!(duration);
-    dbg!(n as f64 / duration.as_secs_f64());
+    dbg!(opts.n_requests as f64 / duration.as_secs_f64());
 
     Ok(())
 }
 
-async fn work_tasks<T, I: IntoIterator<Item = impl std::future::Future<Output = T>>>(
+async fn work<T, I: IntoIterator<Item = impl std::future::Future<Output = T>>>(
     tasks: I,
     n_workers: usize,
 ) -> Vec<Vec<T>> {
@@ -53,6 +55,22 @@ async fn work_tasks<T, I: IntoIterator<Item = impl std::future::Future<Output = 
         let mut ret = Vec::new();
         while let crossbeam::deque::Steal::Success(w) = injector.steal() {
             ret.push(w.await);
+        }
+        ret
+    }))
+    .await
+}
+
+async fn work_duration<T, F: std::future::Future<Output = T>>(
+    task_generator: impl Fn() -> F,
+    duration: std::time::Duration,
+    n_workers: usize,
+) -> Vec<Vec<T>> {
+    let start = std::time::Instant::now();
+    futures::future::join_all((0..n_workers).map(|_| async {
+        let mut ret = Vec::new();
+        while (std::time::Instant::now() - start) < duration {
+            ret.push(task_generator().await);
         }
         ret
     }))
