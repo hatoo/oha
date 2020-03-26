141b25012b0a5d907ba0942731ad4cf50beb5d2c
hatoo <hato2000@gmail.com>
2020-03-26 20:28:19 +0900

    save

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,5 @@
+use anyhow::Context;
+use rand::seq::SliceRandom;
 use tokio::prelude::*;
 use url::Url;
 
@@ -29,8 +31,24 @@ pub struct Client {
 }
 
 impl Client {
-    pub fn lookup_ip(&mut self) -> anyhow::Result<std::net::IpAddr> {
-        todo!()
+    pub async fn lookup_ip(&mut self) -> anyhow::Result<std::net::IpAddr> {
+        let resolver = if let Some(resolver) = self.resolver.take() {
+            resolver
+        } else {
+            trust_dns_resolver::AsyncResolver::tokio(Default::default(), Default::default()).await?
+        };
+
+        let addrs = resolver
+            .lookup_ip(self.url.host_str().context("get host")?)
+            .await?
+            .iter()
+            .collect::<Vec<_>>();
+
+        let addr = *addrs.choose(&mut self.rng).context("get addr")?;
+
+        self.resolver = Some(resolver);
+
+        Ok(addr)
     }
 
     pub fn work(&mut self) -> anyhow::Result<crate::RequestResult> {
