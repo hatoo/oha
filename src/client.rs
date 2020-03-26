bc8272cee8562a26a23c5ba2d3cdb3d3e9341d46
hatoo <hato2000@gmail.com>
2020-03-26 20:03:23 +0900

    save

--- src/client.rs
+++ src/client.rs
@@ -8,18 +8,31 @@ pub struct ClientBuilder {
     url: Url,
 }
 
+impl ClientBuilder {
+    pub fn build(&self) -> Client {
+        todo!()
+    }
+}
+
 pub struct Client {
     url: Url,
-    resolver: trust_dns_resolver::AsyncResolver<
-        trust_dns_resolver::name_server::GenericConnection,
-        trust_dns_resolver::name_server::GenericConnectionProvider<
-            trust_dns_resolver::name_server::TokioRuntime,
+    rng: rand::rngs::ThreadRng,
+    resolver: Option<
+        trust_dns_resolver::AsyncResolver<
+            trust_dns_resolver::name_server::GenericConnection,
+            trust_dns_resolver::name_server::GenericConnectionProvider<
+                trust_dns_resolver::name_server::TokioRuntime,
+            >,
         >,
     >,
     conn: Option<hyper::client::conn::SendRequest<hyper::Body>>,
 }
 
 impl Client {
+    pub fn lookup_ip(&mut self) -> anyhow::Result<std::net::IpAddr> {
+        todo!()
+    }
+
     pub fn work(&mut self) -> anyhow::Result<crate::RequestResult> {
         todo!()
     }
