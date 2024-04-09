f962a93ccf796b9a3b5fd3747fa533c5f65dc1a1
hatoo <hato2000@gmail.com>
2024-04-09 21:52:11 +0900

    test

--- src/result_data.rs
+++ src/result_data.rs
@@ -151,3 +151,163 @@ impl ResultData {
             .map(|r| r.duration())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use float_cmp::assert_approx_eq;
+
+    use super::*;
+    use crate::client::{ClientError, ConnectionTime, RequestResult};
+    use std::time::{Duration, Instant};
+
+    fn build_mock_request_result(
+        status: StatusCode,
+        request_time: u64,
+        connection_time_dns_lookup: u64,
+        connection_time_dialup: u64,
+        size: usize,
+    ) -> Result<RequestResult, ClientError> {
+        let now = Instant::now();
+        Ok(RequestResult {
+            start_latency_correction: None,
+            start: now,
+            connection_time: Some(ConnectionTime {
+                dns_lookup: now
+                    .checked_add(Duration::from_millis(connection_time_dns_lookup))
+                    .unwrap(),
+                dialup: now
+                    .checked_add(Duration::from_millis(connection_time_dialup))
+                    .unwrap(),
+            }),
+            end: now
+                .checked_add(Duration::from_millis(request_time))
+                .unwrap(),
+            status,
+            len_bytes: size,
+        })
+    }
+
+    fn build_mock_request_results() -> ResultData {
+        let mut results = ResultData::default();
+
+        results.push(build_mock_request_result(
+            StatusCode::OK,
+            1000,
+            200,
+            50,
+            100,
+        ));
+        results.push(build_mock_request_result(
+            StatusCode::BAD_REQUEST,
+            100000,
+            250,
+            100,
+            200,
+        ));
+        results.push(build_mock_request_result(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            1000000,
+            300,
+            150,
+            300,
+        ));
+        results
+    }
+
+    #[test]
+    fn test_calculate_success_rate() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.success_rate(), 1.0);
+    }
+
+    #[test]
+    fn test_calculate_slowest_request() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.latency_stat().max(), 1000.0);
+    }
+
+    #[test]
+    fn test_calculate_average_request() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.latency_stat().mean(), 367.0);
+    }
+
+    #[test]
+    fn test_calculate_total_data() {
+        let res = build_mock_request_results();
+        assert_eq!(res.total_data(), 600);
+    }
+
+    #[test]
+    fn test_calculate_size_per_request() {
+        let res = build_mock_request_results();
+        assert_eq!(res.size_per_request(), Some(200));
+    }
+
+    #[test]
+    fn test_calculate_connection_times_dns_dialup_average() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.dns_dialup_stat().mean(), 0.1);
+    }
+
+    #[test]
+    fn test_calculate_connection_times_dns_dialup_fastest() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.dns_dialup_stat().min(), 0.05);
+    }
+
+    #[test]
+    fn test_calculate_connection_times_dns_dialup_slowest() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.dns_dialup_stat().max(), 0.15);
+    }
+
+    #[test]
+    fn test_calculate_connection_times_dns_lookup_average() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.dns_lookup_stat().mean(), 0.25);
+    }
+
+    #[test]
+    fn test_calculate_connection_times_dns_lookup_fastest() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.dns_lookup_stat().min(), 0.2);
+    }
+
+    #[test]
+    fn test_calculate_connection_times_dns_lookup_slowest() {
+        let res = build_mock_request_results();
+        assert_approx_eq!(f64, res.dns_lookup_stat().max(), 0.3);
+    }
+
+    #[test]
+    fn test_get_durations_all() {
+        let res = build_mock_request_results();
+        let durations: Vec<_> = res.duration_all().map(|d| d.as_secs_f64()).collect();
+        assert_approx_eq!(f64, durations[0], 1.0);
+        assert_approx_eq!(f64, durations[1], 100.0);
+        assert_approx_eq!(f64, durations[2], 1000.0);
+    }
+
+    #[test]
+    fn test_get_durations_successful() {
+        let res = build_mock_request_results();
+        let durations: Vec<_> = res.duration_successful().map(|d| d.as_secs_f64()).collect();
+        // Round the calculations to 4 decimal places to remove imprecision
+        assert_approx_eq!(f64, durations[0], 1.0);
+        assert_eq!(durations.get(1), None);
+    }
+
+    #[test]
+    fn test_get_durations_not_successful() {
+        let res = build_mock_request_results();
+        let durations: Vec<_> = res
+            .duration_not_successful()
+            .map(|d| d.as_secs_f64())
+            .collect();
+        // Round the calculations to 4 decimal places to remove imprecision
+        assert_approx_eq!(f64, durations[0], 100.0);
+        assert_approx_eq!(f64, durations[1], 1000.0);
+        assert_eq!(durations.get(2), None);
+    }
+}
