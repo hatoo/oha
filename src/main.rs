6f62b8beb30ad727f007871e4fa87715ed17cd38
hatoo <hato2000@gmail.com>
2024-11-28 16:49:08 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -286,19 +286,23 @@ impl FromStr for VsockAddr {
 async fn main() -> anyhow::Result<()> {
     let mut opts: Opts = Opts::parse();
 
-    let http_version: http::Version = match (opts.http2, opts.http_version) {
+    let parse_http_version = |is_http2: bool, version: Option<&str>| match (is_http2, version) {
         (true, Some(_)) => anyhow::bail!("--http2 and --http-version are exclusive"),
-        (true, None) => http::Version::HTTP_2,
+        (true, None) => Ok(http::Version::HTTP_2),
         (false, Some(http_version)) => match http_version.trim() {
-            "0.9" => http::Version::HTTP_09,
-            "1.0" => http::Version::HTTP_10,
-            "1.1" => http::Version::HTTP_11,
-            "2.0" | "2" => http::Version::HTTP_2,
+            "0.9" => Ok(http::Version::HTTP_09),
+            "1.0" => Ok(http::Version::HTTP_10),
+            "1.1" => Ok(http::Version::HTTP_11),
+            "2.0" | "2" => Ok(http::Version::HTTP_2),
             "3.0" | "3" => anyhow::bail!("HTTP/3 is not supported yet."),
             _ => anyhow::bail!("Unknown HTTP version. Valid versions are 0.9, 1.0, 1.1, 2."),
         },
-        (false, None) => http::Version::HTTP_11,
+        (false, None) => Ok(http::Version::HTTP_11),
     };
+
+    let http_version: http::Version = parse_http_version(opts.http2, opts.http_version.as_deref())?;
+    let proxy_http_version: http::Version =
+        parse_http_version(opts.proxy_http2, opts.proxy_http_version.as_deref())?;
 
     let url_generator = if opts.rand_regex_url {
         // Almost URL has dot in domain, so disable dot in regex for convenience.
@@ -455,11 +459,7 @@ async fn main() -> anyhow::Result<()> {
 
     let client = Arc::new(client::Client {
         http_version,
-        proxy_http_version: if opts.proxy_http2 {
-            http::Version::HTTP_2
-        } else {
-            http::Version::HTTP_11
-        },
+        proxy_http_version,
         url_generator,
         method: opts.method,
         headers,
