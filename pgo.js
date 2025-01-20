576ee24abdf31f586d39b5d35be9ad397e4b3761
hatoo <hato2000@gmail.com>
2025-01-20 21:35:44 +0900

    please success

--- pgo.js
+++ pgo.js
@@ -10,7 +10,8 @@ let server = null;
 
 try {
     server = Bun.spawn(['cargo', 'run', '--release', '--manifest-path', 'pgo/server/Cargo.toml']);
-    await $`cargo pgo run -- --profile pgo ${additional} -- -z 3m -c 900 --no-tui http://localhost:8888`;
+    await $`cargo pgo run -- --profile pgo ${additional} -- -z 2m -c 900 --no-tui http://localhost:8888`;
+    await $`cargo pgo run -- --profile pgo ${additional} -- -n 10000000 -c 900 --no-tui http://localhost:8888`;
     await $`cargo pgo optimize build -- --profile pgo ${additional}`
 } finally {
     if (server !== null) {
