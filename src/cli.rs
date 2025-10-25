6e0b125bc93b9dc115434e2c632b3a219e11126d
violin <reallyviolin@gmail.com>
2025-10-25 20:45:47 +0800

    style: fix clippy error

--- src/cli.rs
+++ src/cli.rs
@@ -2,11 +2,11 @@ pub fn parse_n_requests(s: &str) -> Result<usize, String> {
     let s = s.trim().to_lowercase();
     if let Some(num) = s.strip_suffix('k') {
         num.parse::<f64>()
-            .map(|n| (n * 1000 as f64) as usize)
+            .map(|n| (n * 1000_f64) as usize)
             .map_err(|e| e.to_string())
     } else if let Some(num) = s.strip_suffix('m') {
         num.parse::<f64>()
-            .map(|n| (n * 1_000_000 as f64) as usize)
+            .map(|n| (n * 1_000_000_f64) as usize)
             .map_err(|e| e.to_string())
     } else {
         s.parse::<usize>().map_err(|e| e.to_string())
