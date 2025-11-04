bba593ba3e35fd29a0b40b6f83e89f4d5738272f
hatoo <hato2000@gmail.com>
2025-11-04 21:59:49 +0900

    update test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1241,8 +1241,8 @@ async fn test_csv_output() {
         let current_start = f64::from_str(parts[0]).unwrap();
         assert!(current_start >= latest_start);
         latest_start = current_start;
-        assert!(f64::from_str(parts[1]).unwrap() >= 0f64);
-        assert!(f64::from_str(parts[2]).unwrap() >= 0f64);
+        assert!(f64::from_str(parts[1]).unwrap() > 0f64);
+        assert!(f64::from_str(parts[2]).unwrap() > 0f64);
         assert!(f64::from_str(parts[3]).unwrap() > 0f64);
         assert!(f64::from_str(parts[4]).unwrap() > 0f64);
         assert_eq!(usize::from_str(parts[5]).unwrap(), 11);
